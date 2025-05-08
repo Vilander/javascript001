7ff01ba84dbec07a355eef604dc9194195fbc5f0
Vilander Costa <vilander.costa@gmail.com>
2025-05-08 20:15:03 -0300

    operadores p1

--- app01.js
+++ app01.js
@@ -34,10 +34,38 @@ console.log(typeof salario)
 console.log(typeof fumante)
 
 // Operadores
-//     Operador Atribuição
-//     Operadores aritméticos
-//     Operadores relacionais
-//     Operadores lógicos
+//     Operador Atribuição ( = )
+//     Operadores aritméticos (+,-,*,/,%)
+console.log("10 + 15\n")
+
+let n1 = 10
+let n2 = 5
+
+console.log(n1 + n2)
+console.log(n1 - n2)
+console.log(n1 * n2)
+console.log(n1 / n2)
+console.log(n1 % 3)
+console.log("\n")
+
+//     Operadores relacionais (>,<,>=,<=,==,===,!=,!==)
+console.log(n1 == n2) //igualdade
+console.log(n1 != n2) //diferente
+console.log(n1 > n2) //maior
+console.log(10 < 10) //menor
+console.log(10<=10) //menor ou igual
+console.log(150>=175) //maior ou igual
+
+
+//     Operadores lógicos (!, &&, ||)
+console.log("\n")
+console.log(!10>2) //não => inverte o resultado, se for verdadeiro(true) passa a ser falso(false), se for falso(false) passa a ser verdadeiro(true)
+console.log(!false) //não
+
+console.log(10>2 && 35<100) //e
+console.log(10>2 && 35<100 && n1>n2 && 100<90) //e => todas as verificações devem ser verdadeiras para o resultado ser verdadeiro(true)
+
+console.log(10<2 || 100==150 || 57==57) //ou => basta uma das verificações ser verdadeira(true) para o resultado ser verdadeiro(true), o resultado só será falso(false) se TODAS as verificações forem falsas(false)
 
 // Estrutura
 //     Estrutura de controle/decisão
